4ea022c630333d14c3b4edced3f0ec8fd49f9ae4
fleshdroppp <khusnutdinovakim@gmail.com>
2021-07-01 12:11:54 +0000

    upd router.js and productcontroller.js

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -1,37 +1,50 @@
 import Product from '../Schemas/product.js';
 import mongoose from 'mongoose';
+import e from 'express';
 
 class ProductController {
     async create(req, res) {
         try {
             const { sku, name, type, price } = req.body
             const product = await Product.create({ sku, name, type, price })
-            res.json({ id: product.id })
+            return res.json({ id: product.id })
         } catch (error) {
-            res.status(500).json(error)
+            return res.status(500).json(error)
         }
     }
+
     async getAll(req, res) {
         try {
-            const products = await Product.find()
-            return res.json(products)
+            const limit = 10
+            let products = []
+            let page = Number(req.params.page)
+            if (page === undefined) {
+                products = await Product.find()
+            } else if (page >= 0) {
+                products = await Product.find().skip(limit * page).limit(limit)
+            } else {
+                return res.status(400).json("Not valid page!")
+            }
+            return res.json(products || "Empty array of items!")
         } catch (error) {
             res.status(500).json(error)
         }
     }
+
     async getOne(req, res) {
         try {
-            let product = {};
+            let product = {}
             if (mongoose.isValidObjectId(req.params.id)) {
                 product = await Product.findById(req.params.id)
             } else {
                 product = await Product.findOne({ sku: req.params.id })
             }
-            return res.json(product)
+            return res.json(product || "Can't find item!")
         } catch (error) {
             res.status(500).json(error)
         }
     }
+
     async update(req, res) {
         try {
             let newData = req.body
@@ -43,9 +56,23 @@ class ProductController {
                     product = await Product.findOneAndUpdate({ sku: req.params.id }, newData, { new: true, runValidators: true, context: 'query' })
                 }
             } else {
-                return res.status(400).json("Can't update SKU!")
+                return res.status(400).json("Can't update SKU field!")
             }
-            return res.json(product)
+            return res.json(product || "Can't find item by ID or SKU!")
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            let product = {}
+            if (mongoose.isValidObjectId(req.params.id)) {
+                product = await Product.findByIdAndDelete(req.params.id)
+            } else {
+                product = await Product.findOneAndDelete({ sku: req.params.id })
+            }
+            return res.json(product || "Can't delete product!")
         } catch (error) {
             res.status(500).json(error)
         }
--- router.js
+++ router.js
@@ -3,10 +3,12 @@ import ProductController from './Controllers/ProductController.js'
 
 const router = Router()
 
-router.get('/products', ProductController.getAll)
-router.get('/products/:id', ProductController.getOne)
-router.post('/products', ProductController.create)
-router.put('/products/:id', ProductController.update)
+router.get('/products/all/:page', ProductController.getAll)
+router.get('/products/all/', ProductController.getAll)
+router.get('/products/one/:id', ProductController.getOne)
+router.post('/products/create', ProductController.create)
+router.put('/products/update/:id', ProductController.update)
+router.delete('/products/delete/:id', ProductController.delete)
 
 
 export default router
